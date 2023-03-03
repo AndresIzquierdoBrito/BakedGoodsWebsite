d093d6a1b1fd79c07e750066d4da0ad98820d299
Andres Izquierdo <andres.izbri@gmail.com>
2023-03-03 03:58:05 +0000

    Added files of website

---
+++ js/scripts.js
@@ -0,0 +1,51 @@
+document.addEventListener('DOMContentLoaded', function() {
+    
+    const clickableTexts = document.querySelectorAll('.clickable-text');
+    
+    clickableTexts.forEach(clickableText => {
+        const hiddenDiv = clickableText.nextElementSibling;
+        
+        clickableText.addEventListener('click', function() {
+            hiddenDiv.classList.toggle('hidden');
+            
+        });
+    });
+
+    const icons = document.querySelectorAll('svg');
+    icons.forEach(function(icon) {
+        icon.addEventListener('click', function(event) {
+            event.stopPropagation();
+            event.preventDefault();
+            icon.classList.toggle('checked');
+        });
+    });
+
+    const hamburgerMenu = document.querySelector('.hamburger-menu');
+    const aside = document.querySelector('.aside');
+    const article = document.querySelector('.articles');
+    const originalWidth = article.offsetWidth + 'px';
+    const pages = document.querySelector('.pages');
+    const minidata = document.querySelectorAll('.miniData');
+    
+    hamburgerMenu.addEventListener('click', () => {        
+        aside.classList.toggle('show');
+        article.classList.toggle('article-active');
+        
+        if (article.style.width === '67%') {
+            article.style.width = originalWidth;
+        } else {
+            article.style.width = '67%';
+        }
+        minidata.forEach(mini => {
+            if (article.style.width === '67%') {
+                mini.style.display = 'none';
+            } else {
+                mini.style.display = 'block';
+            }
+        if (pages.style.width === '67%') {
+            pages.style.width = originalWidth;
+        } else {
+            pages.style.width = '67%';
+        }
+    });
+});});
